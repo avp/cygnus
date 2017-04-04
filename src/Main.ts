70127533498306432331f20dfcfcf5aa6d3158c9
Aakash Patel <aakpat6@gmail.com>
2017-04-03 22:21:19 -0700

    Initial commit.

---
+++ src/Main.ts
@@ -0,0 +1,80 @@
+/// <reference path="../typings/index.d.ts" />
+
+import {
+  BrowserWindow,
+  Menu,
+  dialog,
+} from 'electron';
+import {readFile} from 'fs';
+
+export default class Main {
+  static mainWindow: Electron.BrowserWindow;
+  static application: Electron.App;
+  static BrowserWindow;
+
+  private static onWindowAllClosed() {
+    if (process.platform !== 'darwin') {
+      Main.application.quit();
+    }
+  }
+
+  private static onClose() {
+    // Dereference the window object.
+    Main.mainWindow = null;
+  }
+
+  private static openFile() {
+    dialog.showOpenDialog(Main.mainWindow, {
+    }, (files: string[]) => {
+      if (files.length == 0) {
+        return;
+      }
+      readFile(files[0], (err, data) => {
+        if (err) {
+          return console.error(err);
+        }
+        console.log(data);
+      });
+    });
+  }
+
+  private static makeAppMenu() {
+    const template = [
+      {
+        label: 'File',
+        submenu: [
+          {
+            label: 'Open',
+            accelerator: 'CommandOrControl+O',
+            click:  Main.openFile
+          }
+        ],
+      },
+    ];
+    let appMenu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(appMenu);
+  }
+
+  private static onReady() {
+    Main.mainWindow = new Main.BrowserWindow({width: 800, height: 600})
+    Main.mainWindow.maximize();
+    Main.mainWindow.loadURL('file://' + __dirname + '/index.html');
+    Main.mainWindow.isFullScreen();
+    Main.mainWindow.on('closed', Main.onClose);
+    Main.makeAppMenu();
+  }
+
+  static main(
+    app: Electron.App,
+    browserWindow: typeof BrowserWindow){
+    // we pass the Electron.App object and the
+    // Electron.BrowserWindow into this function
+    // so this class1 has no dependencies.  This
+    // makes the code easier to write tests for
+
+    Main.BrowserWindow = browserWindow;
+    Main.application = app;
+    Main.application.on('window-all-closed', Main.onWindowAllClosed);
+    Main.application.on('ready', Main.onReady);
+  }
+}
