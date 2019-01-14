3bf7adf80767ba4249430ecb9234abb230c05496
Aakash Patel <aakpat6@gmail.com>
2019-01-13 21:32:29 -0800

    Update FilledLabel.cpp

--- src/FilledLabel.cpp
+++ src/FilledLabel.cpp
@@ -6,23 +6,18 @@ namespace cygnus {
 void FilledLabel::resizeEvent(QResizeEvent *event) {
   QLabel::resizeEvent(event);
 
-  const auto &cRect = contentsRect();
-  uint32_t fontSize = 1;
-
   QFont font = this->font();
-  while (1) {
-    font.setPixelSize(fontSize);
-    QRect r = QFontMetrics(font).boundingRect(text().isEmpty() ? "A" : text());
-    if (r.height() < cRect.height() && r.width() < cRect.width()) {
-      fontSize++;
-    } else {
-      uint32_t newSize = std::max(
-          text().size() > 1 ? fontSize / 2 : fontSize - 1, (uint32_t)1);
-      font.setPixelSize(newSize);
-      setFont(font);
-      return;
-    }
-  }
+  QRect r = QFontMetrics(font).boundingRect(text().isEmpty() ? "A" : text());
+
+  const auto &cRect = contentsRect();
+
+  qreal scaleW = (qreal)cRect.width() / (qreal)r.width();
+  qreal scaleH = (qreal)cRect.height() / (qreal)r.height();
+
+  qreal scale = std::min(scaleW, scaleH);
+  qDebug() << scale << font.pointSizeF() << font.pointSize();
+  font.setPointSize(font.pointSizeF() * scale);
+  setFont(font);
 }
 
 } // namespace cygnus
